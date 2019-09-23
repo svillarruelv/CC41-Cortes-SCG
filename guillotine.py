7e86580647facb411dcd6944e08a0a9ef4917af3
Ralc <rolozanocampos@hotmail.com>
2019-09-22 23:09:28 -0500

    Logica 1.0

--- guillotine.py
+++ guillotine.py
@@ -1,6 +1,8 @@
 import util as u
 import Rectangle as r
 import DataIn as di
+import Graphics as g
+
 
 """
 r1 = r.Rectangle(0, 0,"A", 40, 10)
@@ -26,12 +28,37 @@ H = 670
 
 """
 
-W, H, N, rectangles = di.Read(demo.txt)
+W, H, N, rectangles = di.Read("demo.txt")
 u.merge_sort(rectangles)
 
+g.set_draws(W,H,rectangles)
+
+g.draw()
+
 def guillotine(W, H, N, rectangles):
+
+    if (N < 1):
+        g.set_draws(W, H, rectangles)
+        g.draw()
+        return
+    
     aux = rectangles[0]
     rectangles = rectangles[1:]
 
+    aux.x = 0
+    aux.y = 0
+    aux.xx = aux.x + aux.w
+    aux.yy = aux.y + aux.h
+    actualx = aux
+    actualy = aux
+
+    cutted = []
+    cutted.append(aux))
+
+    for i in range len(rectangulos):
+
+        if (u.fit(rectangles[i]), actualx, actualy, W, H):
 
 
+
+    fit(aux, W, H)
